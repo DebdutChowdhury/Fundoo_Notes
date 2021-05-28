0a0543a6321a37b7734e39c2c21f8f3a72f5f5fe
DebdutChowdhury <debdut.chowdhury2018@gmail.com>
2021-05-28 21:51:05 +0530

    Added: GetNote component for connect all notes to Dashboard

---
+++ src/Component/GetNote/GetNote.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+import NoteService from '../../Services/NoteService';
+import CreateNote from '../CreateNote/CreateNote'
+import DisplayNotes from '../DisplayNote/DisplayNotes';
+
+const noteService = new NoteService()
+
+export default class GetNote extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            notes: []
+        }
+        
+    }
+    componentDidMount(){
+        this.getNote();
+    }
+
+    getNote = () => {
+        let token = localStorage.getItem('Token')
+        console.log('called');
+        noteService.getNote(token).then((result) => {
+            this.setState({notes:result.data.data.data})
+            console.log(this.state.notes);
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <CreateNote updateData={this.getNote}/>
+                <DisplayNotes updateNote={this.props.updateNote} NotesArray={this.state.notes}/>
+                {/* <DisplayNotes/> */}
+
+            </div>
+        )
+    }
+}
